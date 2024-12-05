efe5f0e507cc80fb05c1ce7cbe5b7231c8de08b9
symicy <alexsima141@gmail.com>
2024-12-05 20:15:27 +0200

    incercari

--- ProblemaCanibalilor/src/Operatori.java
+++ ProblemaCanibalilor/src/Operatori.java
@@ -7,6 +7,7 @@ public class Operatori
 {
     private static final int[][] MUTARI_POSIBILE = {{1, 0}, {0, 1}, {1, 1}, {2, 0}, {0, 2}};
     private static final Random random = new Random();
+
     public static Individ selectieTurnir(List<Individ> populatie, int k)
     {
         // Alegem un subset de k indivizi
@@ -16,7 +17,7 @@ public class Operatori
             int indexAleator = random.nextInt(populatie.size());
             turnir.add(populatie.get(indexAleator));
         }
-        // Găsim cel mai bun individ din turnir (cel cu fitness-ul cel mai mare)
+        // Gasim cel mai bun individ din turnir (cel cu fitness-ul cel mai mare)
         turnir.sort((individ1, individ2) -> Double.compare(individ2.getFitness(), individ1.getFitness()));
         Individ celMaiBun = turnir.getFirst();
         return celMaiBun;
@@ -48,15 +49,47 @@ public class Operatori
         return List.of(copil1, copil2);
     }
 
+    public static List<Individ> incrucisare2Puncte(Individ individ1, Individ individ2)
+    {
+        // Incrucisarea a doi parinti
+        List<Stare> copilIndivid1 = new ArrayList<>();
+        List<Stare> copilIndivid2 = new ArrayList<>();
+        Individ copil1 = new Individ();
+        Individ copil2 = new Individ();
+        int punctIncrucisare1 = random.nextInt(individ1.getIndivid().size() - 1);
+        int punctIncrucisare2 = punctIncrucisare1 + random.nextInt(individ1.getIndivid().size() - punctIncrucisare1);
+        for (int i = 0; i < individ1.getIndivid().size(); i++)
+        {
+            if (i < punctIncrucisare1 || i >= punctIncrucisare2)
+            {
+                copilIndivid1.add(individ1.getIndivid().get(i));
+                copilIndivid2.add(individ2.getIndivid().get(i));
+            }
+            else
+            {
+                copilIndivid1.add(individ2.getIndivid().get(i));
+                copilIndivid2.add(individ1.getIndivid().get(i));
+            }
+        }
+        copil1.setIndivid(copilIndivid1);
+        copil2.setIndivid(copilIndivid2);
+        return List.of(copil1, copil2);
+    }
+
     public static void mutatie(Individ individ, int sansaMutatie)
     {
         // Mutatie a unui individ
         int sansa = random.nextInt(100);
         if (sansa <= sansaMutatie)
         {
-            int pozitieMutatie = random.nextInt(individ.getIndivid().size());
+            int pozitieMutatie = random.nextInt(individ.getIndivid().size()-1);
             int[] mutare = MUTARI_POSIBILE[random.nextInt(MUTARI_POSIBILE.length)];
+            while (individ.getIndivid().get(pozitieMutatie).getCB() == mutare[0] && individ.getIndivid().get(pozitieMutatie).getMB() == mutare[1])
+            {
+                mutare = MUTARI_POSIBILE[random.nextInt(MUTARI_POSIBILE.length)];
+            }
             individ.getIndivid().set(pozitieMutatie, new Stare(mutare[0], mutare[1]));
         }
     }
+
 }
